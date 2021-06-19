e7e71a93fde75b3ccaa7acecf28a90de4e381593
sin <must.sow.before.you.can.reap+0001@gmail.com>
2021-06-19 23:28:23 +0900

    update.

--- konva/pages/Index.tsx
+++ konva/pages/Index.tsx
@@ -1,18 +1,21 @@
-import { createObjectURL } from '../../../lib/image';
 import { Stage, Layer, Image } from 'react-konva';
 import useImage from 'use-image';
-import { isWindow } from '../../../lib/globalUtil';
+import { isWindow } from '../lib/globalUtil';
+
+const url = 'sample.jpg';
 
 const Index = () => {
   const [image] = useImage(url, 'Anonymous');
   return (
-    {isWindow && (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Image image={image} alt="preview-image" />
-        </Layer>
-      </Stage>
-    )}
+    <>
+      {isWindow && (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Image image={image} alt="preview-image" />
+          </Layer>
+        </Stage>
+      )}
+    </>
   );
 };
 
